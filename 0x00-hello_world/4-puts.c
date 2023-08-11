be4d5891c1296ba04999592641f7efd3e1385b24
Olatemiju Ezekiel <olatemijuezekiel21@gmail.com>
2023-08-11 14:19:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main -it prints out output using put
  *
- * Return: Always 0 (Success)
+ * Return: always
  * */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
