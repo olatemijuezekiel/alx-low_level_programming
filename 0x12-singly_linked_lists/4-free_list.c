296e3653eaf06270628d84be20bf1da82a656494
Olatemiju Ezekiel <olatemijuezekiel21@gmail.com>
2023-09-21 18:20:19 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -17,37 +17,3 @@ void free_list(list_t *head)
 		head = temp;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-100-first.c
-
-#include <stdio.h>
-
-void first(void) __attribute__ ((constructor));
-
-/**
- * first - prints a sentence before the main
- * function is executed
- */
-void first(void)
-{
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
-}
